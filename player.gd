b08033127e752b1e1f8e19abeb5a56c0b4e6739a
Paul Winkler <slinkp@gmail.com>
2023-07-17 16:41:43 -0700

    Add code to move the player per Coding the Player tutorial

--- player.gd
+++ player.gd
@@ -11,4 +11,28 @@ func _ready():
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO
+	# Allow pressing more than one key and add the results
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		# Normalizing fixes diagonals being faster.
+		# more at https://docs.godotengine.org/en/stable/tutorials/math/vector_math.html#doc-vector-math
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		# No animation unless moving
+		$AnimatedSprite2D.stop()
+
+	position += velocity * delta
+
+	# Don't allow going off screen!
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
